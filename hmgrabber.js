b8e8a791f69ba4064f741580c7fff8e1088c520b
jimsafley <jimsafley@gmail.com>
2013-01-17 11:24:52 -0800

    Create hmgrabber.js

---
+++ hmgrabber.js
@@ -0,0 +1,5 @@
+var hmgTrack = window.playList['tracks'][currentTrack];
+xmlHttp = new XMLHttpRequest();
+xmlHttp.open('GET', 'http://hypem.com/serve/source/' + hmgTrack.id + '/' + hmgTrack.key, false);
+xmlHttp.send(null);
+window.open(null, 'hmg', 'width=400,height=600').document.write('<a href="' + JSON.parse(xmlHttp.responseText).url + '">' + hmgTrack.song + '.</a> by ' + hmgTrack.artist + '<br>');
